d1f4398c0caa21b055547aa332fdce540e086cd7
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-03-20 18:51:04 +0200

    Update elev.h

--- elev.h
+++ elev.h
@@ -4,15 +4,23 @@
 
 class Elev
 {
-    public:
-        std::string Nume;
-        int clasa;
-        std::vector<std::string> Algorithms, Problems;
+    std::string Nume;
+    int clasa;
+    std::vector<std::string> Algorithms, Problems;
 
+    public:
         Elev(std::string a, int b)
         {
             clasa = b;
             Nume = a;
+        }
+        void insertalgo(std::string s)
+        {
+            Algorithms.push_back(s);
+        }
+        std::string getname()
+        {
+            return Nume;
         }
 };
 
