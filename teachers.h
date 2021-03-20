6ef1aa9cfa3b66963b523cb00e5a6975426fc71a
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-03-20 19:12:59 +0200

    Update teachers.h

--- teachers.h
+++ teachers.h
@@ -11,26 +11,9 @@ class Teachers
     std::vector<std::string> feedback_lists;
     std::map<std::string, int> elevi;
     public:
-        void add_feedback(std::string feedback)
-        {
-            feedback_lists.push_back(feedback);
-        }
-        void detailed_feedback(int wh, std::string feedback)
-        {
-            Profesor y = profesori[wh];
-            y.add_feedback(feedback);
-        }
-        void add_teacher(std::string nume, std::string adresa)
-        {
-            Profesor x(nume, adresa);
-            profesori.push_back(x);
-        }
-        Profesor getdata(int nr)
-        {
-            return profesori[nr];
-        }
-        int getstudent(std::string nume_elev)
-        {
-            return elevi[nume_elev];
-        }
+        void add_feedback(std::string feedback);
+        void detailed_feedback(int wh, std::string feedback);
+        void add_teacher(std::string nume, std::string adresa);
+        Profesor getdata(int nr);
+        int getstudent(std::string nume_elev);
 };
