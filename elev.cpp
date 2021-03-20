d0c7e1fa9e4fb03ba1422061026d1ac4c00afc67
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-03-20 19:16:53 +0200

    Update elev.cpp

--- elev.cpp
+++ elev.cpp
@@ -1,2 +1,17 @@
 #include "elev.h"
 
+Elev::Elev(std::string a, int b)
+{
+    clasa = b;
+    Nume = a;
+}
+
+void Elev::insertalgo(std::string s)
+{
+    Algorithms.push_back(s);
+}
+
+std::string Elev::getname()
+{
+    return Nume;
+}
