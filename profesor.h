276c84e499fe5ef6167ee14099daf7b4d18ec787
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-03-20 19:17:38 +0200

    Update profesor.h

--- profesor.h
+++ profesor.h
@@ -14,28 +14,9 @@ class Profesor
     std::map<std::string, int> link;
 
     public:
-        Profesor(std::string b, std::string c)
-        {
-            nr_elevi = 0;
-            nume = b;
-            adresa = c;
-        }
-        void add_feedback(std::string s)
-        {
-            feedback.push_back(s);
-        }
-        void add_elev(Elev e)
-        {
-            elevi.push_back(e);
-            std::string x = e.getname();
-            link[x] = elevi.size() - 1;
-        }
-        int getlink(std::string nume_elev)
-        {
-            return link[nume_elev];
-        }
-        Elev getstudent(int poz)
-        {
-            return elevi[poz];
-        }
+        Profesor(std::string b, std::string c);
+        void add_feedback(std::string s);
+        void add_elev(Elev e);
+        int getlink(std::string nume_elev);
+        Elev getstudent(int poz);
 };
