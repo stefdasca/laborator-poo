98dd09168d7350b3d6a7663f36454d3ceaf4934d
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-03-22 19:36:03 +0200

    Update profesor.h

--- profesor.h
+++ profesor.h
@@ -8,6 +8,8 @@ class Profesor
 {
 
     int nr_elevi;
+    int cnt_feedbacks;
+    int score_feedbacks;
     std::string nume, adresa;
     std::vector<Elev> elevi;
     std::vector<std::string> feedback;
@@ -15,8 +17,17 @@ class Profesor
 
     public:
         Profesor(std::string b, std::string c);
+        ~Profesor();
+        
+        friend std::ostream& operator<<(std::ostream& output, const Profesor& profesor);
+        Profesor operator=(const Profesor& Profesor);
+
         void add_feedback(std::string s);
         void add_elev(Elev e);
         int getlink(std::string nume_elev);
         Elev getstudent(int poz);
+        std::string getname();
+        int rate_feedback(std::string s);
+        void add_score(int val);
+
 };
