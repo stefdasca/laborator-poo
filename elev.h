57dd114b175988f54ad1fd18dae189717b1f6001
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-03-22 19:36:40 +0200

    Update elev.h

--- elev.h
+++ elev.h
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <set>
 
 class Elev
 {
     std::string Nume;
     int clasa;
+    std::string Profesor;
     std::vector<std::string> Algorithms, Problems;
+    std::set<int> tasks;
 
     public:
-        Elev(std::string a, int b);
+        Elev();
+        Elev(std::string a, int b, std::string c);
+        ~Elev();
+        
+        friend std::ostream& operator<<(std::ostream& output, const Elev& elev);
+        Elev operator=(const Elev& elev);
+
         void insertalgo(std::string s);
+        void inserttask(int p);
+        void solvedtask(int p);
         std::string getname();
 };
 
