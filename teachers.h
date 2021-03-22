46c8bbd18e6317d6b622d077cb56c8604d6d65d9
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-03-22 19:35:20 +0200

    Update teachers.h

--- teachers.h
+++ teachers.h
@@ -3,17 +3,28 @@
 #include <string>
 #include <map>
 #include "profesor.h"
+#include "task.h"
 
-class Teachers
+class Firma
 {
 
     std::vector<Profesor> profesori;
     std::vector<std::string> feedback_lists;
     std::map<std::string, int> elevi;
+    std::vector<Task> tasklist;
+
     public:
+        Firma();
+        Firma(std::vector<Profesor> a, std::vector<std::string> b, std::map<std::string, int> c, std::vector<Task> d);
+        ~Firma();
+        
+        friend std::ostream& operator<<(std::ostream& output, const Firma& firma);
+        Firma operator=(const Firma& firma);
+
         void add_feedback(std::string feedback);
         void detailed_feedback(int wh, std::string feedback);
         void add_teacher(std::string nume, std::string adresa);
         Profesor getdata(int nr);
         int getstudent(std::string nume_elev);
+        void add_task(Task tsk);
 };
