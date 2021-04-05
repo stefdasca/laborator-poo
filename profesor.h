44171bb648b06fbc44c5b428fc17dbfe52c5abaa
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-04-06 01:57:35 +0300

    Update profesor.h

--- profesor.h
+++ profesor.h
@@ -2,33 +2,31 @@
 #include <vector>
 #include <string>
 #include <map>
-#include "elev.h"
 
-class Profesor 
+class Profesor
 {
 
     int nr_elevi;
     int cnt_feedbacks;
     int score_feedbacks;
     std::string nume, adresa;
-    std::vector<Elev> elevi;
+    std::vector<int> elevi;
     std::vector<std::string> feedback;
     std::map<std::string, int> link;
 
-    public:
-        Profesor(std::string b, std::string c);
-        ~Profesor();
-        Profesor(const Profesor &prof);
-        
-        friend std::ostream& operator<<(std::ostream& output, const Profesor& profesor);
-        Profesor operator=(const Profesor& Profesor);
+public:
+    Profesor(std::string b, std::string c);
+    ~Profesor();
+    Profesor(const Profesor& prof);
 
-        void add_feedback(std::string s);
-        void add_elev(Elev e);
-        int getlink(std::string nume_elev);
-        Elev getstudent(int poz);
-        std::string getname();
-        int rate_feedback(std::string s);
-        void add_score(int val);
+    friend std::ostream& operator<<(std::ostream& output, const Profesor& profesor);
+    Profesor operator=(const Profesor& Profesor);
+
+    void add_feedback(std::string s);
+    void add_elev(int e);
+    int getlink(std::string nume_elev);
+    std::string getname();
+    int rate_feedback(std::string s);
+    void add_score(int val);
 
 };
