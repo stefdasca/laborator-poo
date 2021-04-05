873e635fcffbb1c789cbb92cf6e0b51f1e1e0c0f
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-04-06 01:57:27 +0300

    Update profesor.cpp

--- profesor.cpp
+++ profesor.cpp
@@ -19,11 +19,6 @@ Profesor::~Profesor()
     score_feedbacks = 0;
     nume = "";
     adresa = "";
-    for(int i = 0; i < (int) elevi.size(); ++i) // distrugerea unui profesor presupune si distrugerea elevilor 
-    {
-        Elev e = getstudent(i);
-        e.~Elev();
-    }
     elevi.clear();
     feedback.clear();
     link.clear();
@@ -43,15 +38,15 @@ Profesor::Profesor(const Profesor& profesor)
 
 Profesor Profesor::operator=(const Profesor& profesor)
 {
-    this -> nr_elevi = profesor.nr_elevi;
-    this -> nume = profesor.nume;
-    this -> adresa = profesor.adresa;
-    this -> elevi = profesor.elevi;
-    this -> feedback = profesor.feedback;
-    this -> link = profesor.link;
-    this -> cnt_feedbacks = profesor.cnt_feedbacks;
-    this -> score_feedbacks = profesor.score_feedbacks;
-    return &this;
+    this->nr_elevi = profesor.nr_elevi;
+    this->nume = profesor.nume;
+    this->adresa = profesor.adresa;
+    this->elevi = profesor.elevi;
+    this->feedback = profesor.feedback;
+    this->link = profesor.link;
+    this->cnt_feedbacks = profesor.cnt_feedbacks;
+    this->score_feedbacks = profesor.score_feedbacks;
+    return *this;
 }
 
 std::ostream& operator<<(std::ostream& output, const Profesor& profesor)
@@ -66,11 +61,9 @@ void Profesor::add_feedback(std::string s)
     feedback.push_back(s);
 }
 
-void Profesor::add_elev(Elev e)
+void Profesor::add_elev(int x)
 {
-    elevi.push_back(e);
-    std::string x = e.getname();
-    link[x] = (int) elevi.size() - 1;
+    elevi.push_back(x);
 }
 
 int Profesor::getlink(std::string nume_elev)
@@ -78,14 +71,10 @@ int Profesor::getlink(std::string nume_elev)
     return link[nume_elev];
 }
 
-Elev Profesor::getstudent(int poz)
-{
-    return elevi[poz-1];
-}
 
 std::string Profesor::getname()
 {
-    return this -> nume;
+    return this->nume;
 }
 
 
@@ -94,17 +83,17 @@ int Profesor::rate_feedback(std::string s) // un scor bazat pe cuvintele ce apar
     int scor = 0;
     std::map<std::string, int> cuvinte;
     cuvinte["bun"] = 1; cuvinte["excelent"] = 2; cuvinte["clare"] = 1; cuvinte["dedicat"] = 2; cuvinte["activ"] = 1; cuvinte["punctual"] = 1;
-    cuvinte["foarte bun"] = 1; cuvinte["organizat"] = 1; cuvinte["adaptabil"] = 1; cuvinte["calitate"] = 1; cuvinte["slab"] = -1; 
+    cuvinte["foarte bun"] = 1; cuvinte["organizat"] = 1; cuvinte["adaptabil"] = 1; cuvinte["calitate"] = 1; cuvinte["slab"] = -1;
     cuvinte["serios"] = 1; cuvinte["neserios"] = -2; cuvinte["multumit"] = 1; cuvinte["nemultumit"] = -2;
     cuvinte["implicat"] = 1; cuvinte["neimplicat"] = -2; cuvinte["lipsa"] = -1; cuvinte["dezinteresat"] = -2;
-    cuvinte["neclare"] = -2; cuvinte["calm"] = 1; 
-    for(int i = 0; i < (int) s.size(); ++i)
+    cuvinte["neclare"] = -2; cuvinte["calm"] = 1;
+    for (int i = 0; i < (int)s.size(); ++i)
     {
         std::string x;
-        for(int j = i; j < std::min((int) s.size(), i + 15); ++j)
+        for (int j = i; j < std::min((int)s.size(), i + 15); ++j)
         {
             x += s[j];
-            if(cuvinte.find(x) != cuvinte.end())
+            if (cuvinte.find(x) != cuvinte.end())
                 scor += cuvinte[x];
         }
     }
@@ -113,6 +102,6 @@ int Profesor::rate_feedback(std::string s) // un scor bazat pe cuvintele ce apar
 
 void Profesor::add_score(int val)
 {
-    this -> cnt_feedbacks++;
-    this -> score_feedbacks += val;
+    this->cnt_feedbacks++;
+    this->score_feedbacks += val;
 }
