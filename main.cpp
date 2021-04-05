48736b227dee177d3b368835c7ad35fe475fd4f1
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-04-05 11:55:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,55 @@
+/*
+    Interfata profesor-elev
+
+    Programul functioneaza preluand datele de intrare din fisierul date.in si prelucreaza datele de intrare, dupa cum urmeaza:
+
+    type = 0: se primeste un feedback general si se adauga la lista de feedbackuri ale companiei
+    type = 1: se primeste un feedback pentru un profesor si se adauga la lista de feedbackuri ale profesorului respectiv
+    type = 2: se primesc detaliile unui profesor si se adauga profesorul in companie
+    type = 3: se primesc detaliile unui elev si se adauga elevul in lista unui profesor
+    type = 4: se primesc detaliile studierii unui algoritm de un elev si se adauga informatia in lista de algoritmi al elevului respectiv
+    type = 5: se primesc detaliile unei probleme de rezolvat si se adauga problema pentru elevul respectiv
+    type = 6: se primesc detaliile unei noi teme de rezolvat si se adauga tema pentru elevul respectiv
+    type = 7: se primesc detaliile unei probleme care a fost rezolvata si se sterge problema pentru elevul respectiv
+
+*/
+
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "firma.h"
 
-int actions;
 
 int main()
 {
-    std::cin >> actions;
+    std::ifstream f("date.in");
+    std::ofstream g("date.out");
+
+    int actions;
+    f >> actions;
     Firma company;
+    
     for(int i = 1; i <= actions; ++i)
     {
         int type;
-        std::cin >> type;
+        f >> type;
         if(type == 0) // feedback general
         {
             std::string feedback;
-            std::cin >> feedback;
+            f >> feedback;
             company.add_feedback(feedback);
         }
         if(type == 1) // feedback pentru un profesor
         {
             int wh;
             std::string feedback;
-            std::cin >> wh >> feedback;
+            f >> wh >> feedback;
             company.detailed_feedback(wh, feedback);
         }
         if(type == 2) // nou profesor
         {
             std::string nume, adresa;
-            std::cin >> nume >> adresa;
+            f >> nume >> adresa;
             company.add_teacher(nume, adresa);
         }
         if(type == 3) // nou elev
@@ -36,7 +57,7 @@ int main()
             std::string nume;
             int clasa;
             int teacher;
-            std::cin >> nume >> clasa >> teacher;
+            f >> nume >> clasa >> teacher;
             Profesor pr = company.getdata(teacher);
             Elev student(nume, clasa, pr.getname());
             pr.add_elev(student);
@@ -44,7 +65,7 @@ int main()
         if(type == 4) // nou algoritm 
         {
             std::string nume_algoritm, nume_elev;
-            std::cin >> nume_elev >> nume_algoritm;
+            f >> nume_elev >> nume_algoritm;
             int val = company.getstudent(nume_elev); 
             Profesor x = company.getdata(val);
             int poz = x.getlink(nume_elev);
@@ -54,16 +75,16 @@ int main()
         if(type == 5) // nou task
         {
             std::string taskname;
-            std::cin >> taskname;
+            f >> taskname;
             int cnttags;
-            std::cin >> cnttags;
+            f >> cnttags;
             std::vector<std::string> tags(cnttags);
             for(int x = 0; x < cnttags; ++x)
-                std::cin >> tags[x];
+                f >> tags[x];
             int difficulty;
-            std::cin >> difficulty;
+            f >> difficulty;
             std::string solution;
-            std::cin >> solution;
+            f >> solution;
             Task problema(taskname, tags, difficulty, solution);
             company.add_task(problema);
         }
@@ -71,7 +92,7 @@ int main()
         {
             std::string nume_elev;
             int task_number;
-            std::cin >> nume_elev >> task_number;
+            f >> nume_elev >> task_number;
             int val = company.getstudent(nume_elev); 
             Profesor x = company.getdata(val);
             int poz = x.getlink(nume_elev);
@@ -83,7 +104,7 @@ int main()
         {
             std::string nume_elev;
             int task_number;
-            std::cin >> nume_elev >> task_number;
+            f >> nume_elev >> task_number;
             int val = company.getstudent(nume_elev); 
             Profesor x = company.getdata(val);
             int poz = x.getlink(nume_elev);
