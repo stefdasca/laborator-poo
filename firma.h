4901285ccf88c7a1ffa4c2ae4371f7f08247ce4b
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-04-06 01:57:12 +0300

    Update firma.h

--- firma.h
+++ firma.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include "elev.h"
 #include "profesor.h"
 #include "task.h"
 
@@ -10,22 +11,26 @@ class Firma
 
     std::vector<Profesor> profesori;
     std::vector<std::string> feedback_lists;
-    std::map<std::string, int> elevi;
+    std::vector<Elev> elevi;
+    std::map<std::string, int> positions;
     std::vector<Task> tasklist;
 
-    public:
-        Firma();
-        Firma(std::vector<Profesor> a, std::vector<std::string> b, std::map<std::string, int> c, std::vector<Task> d);
-        ~Firma();
-        Firma(const Firma &f);
-        
-        friend std::ostream& operator<<(std::ostream& output, const Firma& firma);
-        Firma operator=(const Firma& firma);
+public:
+    Firma();
+    Firma(std::vector<Profesor> a, std::vector<std::string> b, std::vector<Elev> c, std::map<std::string, int> x, std::vector<Task> d);
+    ~Firma();
+    Firma(const Firma& f);
 
-        void add_feedback(std::string feedback);
-        void detailed_feedback(int wh, std::string feedback);
-        void add_teacher(std::string nume, std::string adresa);
-        Profesor getdata(int nr);
-        int getstudent(std::string nume_elev);
-        void add_task(Task tsk);
+    friend std::ostream& operator<<(std::ostream& output, const Firma& firma);
+    Firma operator=(const Firma& firma);
+
+    void add_feedback(std::string feedback);
+    void detailed_feedback(int wh, std::string feedback);
+    void add_teacher(std::string nume, std::string adresa);
+    Profesor getdata(int nr);
+    Elev getstudent(std::string nume_elev);
+    void add_task(Task tsk);
+    void add_student(Elev e);
+    int find_position(std::string nume_elev);
+    void modify_position(int poz, Elev e);
 };
