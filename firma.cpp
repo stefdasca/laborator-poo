35373cff4503d68d1f5e0fb93406b7b5e1935f32
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-04-06 01:56:41 +0300

    Update firma.cpp

--- firma.cpp
+++ firma.cpp
@@ -5,22 +5,25 @@ Firma::Firma()
     profesori.clear();
     feedback_lists.clear();
     elevi.clear();
+    positions.clear();
     tasklist.clear();
 }
 
-Firma::Firma(const Firma &f)
+Firma::Firma(const Firma& f)
 {
     profesori = f.profesori;
     feedback_lists = f.feedback_lists;
     elevi = f.elevi;
+    positions = f.positions;
     tasklist = f.tasklist;
 }
 
-Firma::Firma(std::vector<Profesor> a, std::vector<std::string> b, std::map<std::string, int> c, std::vector<Task> d)
+Firma::Firma(std::vector<Profesor> a, std::vector<std::string> b, std::vector<Elev> c, std::map<std::string, int> x, std::vector<Task> d)
 {
     profesori = a;
     feedback_lists = b;
     elevi = c;
+    positions = x;
     tasklist = d;
 }
 
@@ -33,28 +36,28 @@ Firma::~Firma()
 
 Firma Firma::operator=(const Firma& firma)
 {
-    this -> profesori = firma.profesori;
-    this -> feedback_lists = firma.feedback_lists;
-    this -> elevi = firma.elevi;
-    return &this;
+    this->profesori = firma.profesori;
+    this->feedback_lists = firma.feedback_lists;
+    this->elevi = firma.elevi;
+    return *this;
 }
 
 std::ostream& operator<<(std::ostream& output, const Firma& Firma)
 {
     output << "Lista profesori: " << '\n';
-    for(int i = 0; i < (int) Firma.profesori.size(); ++i)
+    for (int i = 0; i < (int)Firma.profesori.size(); ++i)
     {
         Profesor prof = Firma.profesori[i];
         output << prof.getname() << " ";
     }
     output << '\n';
     output << "Lista feedback: " << '\n';
-    for(int i = 0; i < (int) Firma.feedback_lists.size(); ++i)
+    for (int i = 0; i < (int)Firma.feedback_lists.size(); ++i)
     {
         std::string feedback = Firma.feedback_lists[i];
         output << feedback << " ";
     }
-    return &output;
+    return output;
 }
 
 void Firma::add_teacher(std::string nume, std::string adresa)
@@ -70,7 +73,7 @@ void Firma::add_feedback(std::string feedback)
 
 void Firma::detailed_feedback(int wh, std::string feedback)
 {
-    Profesor y = profesori[wh-1];
+    Profesor y = profesori[wh - 1];
     y.add_feedback(feedback);
     int val = y.rate_feedback(feedback);
     y.add_score(val);
@@ -78,15 +81,31 @@ void Firma::detailed_feedback(int wh, std::string feedback)
 
 Profesor Firma::getdata(int nr)
 {
-    return profesori[nr-1];
+    return profesori[nr - 1];
 }
 
-int Firma::getstudent(std::string nume_elev)
+Elev Firma::getstudent(std::string nume_elev)
 {
-    return elevi[nume_elev];
+    return elevi[positions[nume_elev]];
+}
+
+int Firma::find_position(std::string nume_elev)
+{
+    return positions[nume_elev];
 }
 
 void Firma::add_task(Task tsk)
 {
     tasklist.push_back(tsk);
+}
+
+void Firma::add_student(Elev e)
+{
+    elevi.push_back(e);
+    positions[e.getname()] = (int) elevi.size() - 1;
+}
+
+void Firma::modify_position(int poz, Elev e)
+{
+    elevi[poz] = e;
 }
