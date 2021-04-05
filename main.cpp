6d5dc9a7a342c14cd6182f47676b2a475d420c76
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-04-06 01:56:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 /*
     Interfata profesor-elev
-
     Programul functioneaza preluand datele de intrare din fisierul date.in si prelucreaza datele de intrare, dupa cum urmeaza:
-
     type = 0: se primeste un feedback general si se adauga la lista de feedbackuri ale companiei
     type = 1: se primeste un feedback pentru un profesor si se adauga la lista de feedbackuri ale profesorului respectiv
     type = 2: se primesc detaliile unui profesor si se adauga profesorul in companie
@@ -12,7 +10,6 @@
     type = 6: se primesc detaliile unei noi teme de rezolvat si se adauga tema pentru elevul respectiv
     type = 7: se primesc detaliile unei probleme care a fost rezolvata si se sterge problema pentru elevul respectiv
     type = 8: se primeste numarul de ordine al unui elev si se afiseaza informatiile despre elev
-
 */
 
 #include <iostream>
@@ -29,31 +26,32 @@ int main()
     int actions;
     f >> actions;
     Firma company;
-    
-    for(int i = 1; i <= actions; ++i)
+
+    int cnt_student = 0;
+    for (int i = 1; i <= actions; ++i)
     {
         int type;
         f >> type;
-        if(type == 0) // feedback general
+        if (type == 0) // feedback general
         {
             std::string feedback;
             f >> feedback;
             company.add_feedback(feedback);
         }
-        if(type == 1) // feedback pentru un profesor
+        if (type == 1) // feedback pentru un profesor
         {
             int wh;
             std::string feedback;
             f >> wh >> feedback;
             company.detailed_feedback(wh, feedback);
         }
-        if(type == 2) // nou profesor
+        if (type == 2) // nou profesor
         {
             std::string nume, adresa;
             f >> nume >> adresa;
             company.add_teacher(nume, adresa);
         }
-        if(type == 3) // nou elev
+        if (type == 3) // nou elev
         {
             std::string nume;
             int clasa;
@@ -61,26 +59,25 @@ int main()
             f >> nume >> clasa >> teacher;
             Profesor pr = company.getdata(teacher);
             Elev student(nume, clasa, pr.getname());
-            pr.add_elev(student);
+            ++cnt_student;
+            pr.add_elev(cnt_student);
+            company.add_student(student);
         }
-        if(type == 4) // nou algoritm 
+        if (type == 4) // nou algoritm 
         {
             std::string nume_algoritm, nume_elev;
             f >> nume_elev >> nume_algoritm;
-            int val = company.getstudent(nume_elev); 
-            Profesor x = company.getdata(val);
-            int poz = x.getlink(nume_elev);
-            Elev e = x.getstudent(poz);
+            Elev e = company.getstudent(nume_elev);
             e.insertalgo(nume_algoritm);
         }
-        if(type == 5) // nou task
+        if (type == 5) // nou task
         {
             std::string taskname;
             f >> taskname;
             int cnttags;
             f >> cnttags;
             std::vector<std::string> tags(cnttags);
-            for(int x = 0; x < cnttags; ++x)
+            for (int x = 0; x < cnttags; ++x)
                 f >> tags[x];
             int difficulty;
             f >> difficulty;
@@ -89,38 +86,32 @@ int main()
             Task problema(taskname, tags, difficulty, solution);
             company.add_task(problema);
         }
-        if(type == 6) // tema noua
+        if (type == 6) // tema noua
         {
             std::string nume_elev;
             int task_number;
             f >> nume_elev >> task_number;
-            int val = company.getstudent(nume_elev); 
-            Profesor x = company.getdata(val);
-            int poz = x.getlink(nume_elev);
-            Elev e = x.getstudent(poz);
+            Elev e = company.getstudent(nume_elev);
             e.inserttask(task_number);
         }
 
-        if(type == 7) // un elev a rezolvat o problema
+        if (type == 7) // un elev a rezolvat o problema
         {
             std::string nume_elev;
             int task_number;
             f >> nume_elev >> task_number;
-            int val = company.getstudent(nume_elev); 
-            Profesor x = company.getdata(val);
-            int poz = x.getlink(nume_elev);
-            Elev e = x.getstudent(poz);
+            Elev e = company.getstudent(nume_elev);
             e.solvedtask(task_number);
+            e.update_score(e.get_score() + 1);
+            int poz = company.find_position(nume_elev);
+            company.modify_position(poz, e);
         }
-        if(type == 8) // afisam informatiile despre un elev
+        if (type == 8) // afisam informatiile despre un elev
         {
             std::string nume_elev;
             f >> nume_elev;
-            int val = company.getstudent(nume_elev); 
-            Profesor x = company.getdata(val);
-            int poz = x.getlink(nume_elev);
-            Elev e = x.getstudent(poz);
-            g << e << std::endl;
+            Elev e = company.getstudent(nume_elev);
+            std::cout << e << std::endl;
         }
     }
     return 0;
