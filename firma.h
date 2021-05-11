9571a95934e5fbae1005d6452daa98ec4a3c6009
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-05-11 13:53:35 +0300

    Update firma.h

--- firma.h
+++ firma.h
@@ -6,6 +6,7 @@
 #include "profesor.h"
 #include "task.h"
 
+
 class Firma
 {
 
@@ -13,7 +14,8 @@ class Firma
     std::vector<std::string> feedback_lists;
     std::vector<Elev> elevi;
     std::map<std::string, int> positions;
-    std::vector<Task> tasklist;
+    std::vector<Task> task_list;
+    std::vector<Educational_Task> edu_task_list;
 
 public:
     Firma();
@@ -27,10 +29,12 @@ public:
     void add_feedback(std::string feedback);
     void detailed_feedback(int wh, std::string feedback);
     void add_teacher(std::string nume, std::string adresa);
-    Profesor getdata(int nr);
-    Elev getstudent(std::string nume_elev);
+    Profesor get_data(int nr);
+    Elev get_student(std::string nume_elev);
     void add_task(Task tsk);
     void add_student(Elev e);
     int find_position(std::string nume_elev);
     void modify_position(int poz, Elev e);
+    void add_edu_task(Educational_Task tsk);
+    Educational_Task get_edu_task(int nr);
 };
