ee16d28e9b2647a46cf1836d55248c7dfb141ada
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2021-06-02 17:18:41 +0300

    Update task.h

--- task.h
+++ task.h
@@ -15,7 +15,6 @@ public:
 
     Task();
     Task(std::string a, std::vector<std::string> b, int c, std::string d);
-    ~Task();
     Task(const Task& t);
 
     friend std::ostream& operator<<(std::ostream& output, const Task& task);
@@ -23,7 +22,7 @@ public:
     void set_data(std::string a, std::vector<std::string> b, int c, std::string d);
     virtual void tip_problema() const;
     virtual void add_difficulty(int nr);
-
+    virtual ~Task();
 };
 
 class Educational_Task : public Task
@@ -33,4 +32,5 @@ public:
     std::string get_Help();
     void tip_problema() const override;
     void add_difficulty(int nr) override;
+    ~Educational_Task() override;
 };
